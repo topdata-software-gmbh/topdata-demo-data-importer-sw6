b5fd356c1839932eeed829f82197f22e7c8feac6
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2024-11-02 08:27:50 +0100

    foundation WIP

---
+++ src/Controller/TopdataDemoDataAdminApiController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Topdata\TopdataConnectorSW6\Controller\Admin;
+
+use Shopware\Core\Framework\Context;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Topdata\TopdataDemoDataImportSW6\Service\DemoDataImportService;
+
+/**
+ * 11/2024 extracted from TopdataWebserviceConnectorAdminApiController
+ */
+#[Route(defaults: ['_routeScope' => ['administration']])]
+class TopdataDemoDataAdminApiController extends AbstractController
+{
+
+    public function __construct(
+        private readonly DemoDataImportService $demoDataImportService,
+    )
+    {
+    }
+
+
+    /**
+     * Install demo data.
+     * FIXME: inject the ProductsCommand into the controller (later, make a service, eg DemoDataService)
+     */
+    #[Route(
+        path: '/api/topdata/connector-install-demodata',
+        name: 'api.action.topdata.connector-install-demodata',
+        methods: ['GET']
+    )]
+    public function installDemoData(Request $request, Context $context): JsonResponse
+    {
+        $result = $this->demoDataImportService->installDemoData();
+
+        return new JsonResponse($result);
+    }
+}
